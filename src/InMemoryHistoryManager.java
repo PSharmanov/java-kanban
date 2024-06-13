28f74f95aa7305fe99e1856b8958d65dbf6955c6
PSharmanov <sharmanov88@mail.ru>
2024-06-13 16:11:58 +0500

    6 sprint. update HistoryManager. update test.

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,25 +1,94 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    List<Task> history = new ArrayList<>();
-    private final static int MAX_SIZE_HISTORY = 10;
+    Map<Integer, Node<Task>> history = new HashMap<>();
 
     //метод добавления задач в список
     @Override
     public void addHistory(Task task) {
-        if (history.size() >= MAX_SIZE_HISTORY) {
-            history.remove(0);
+        if (history.containsKey(task.getId())){
+            linkLast(task);
+            removeNode(history.get(task.getId()));
+        } else {
+            linkLast(task);
         }
-
-        history.add(task);
-
     }
 
-    //метод возвращает список последник 10 просмотреных задач
+    //удаление истории по id задачи
+    @Override
+    public void remove(int id) {
+        removeNode(history.get(id));
+        history.remove(id);
+    }
+
+    //метод возвращает список последник задач
     @Override
     public List<Task> getHistory() {
-        return history;
+        return getTask();
+    }
+
+    //класс объектов двусвязного списка истории
+    static class Node<E> {
+        public E task;
+        public Node<E> next;
+        public Node<E> prev;
+
+        public Node(Node<E> prev, E task, Node<E> next) {
+            this.task = task;
+            this.next = next;
+            this.prev = prev;
+
+        }
+    }
+
+    private Node tail;
+
+    //метод добавления ноды в конец списка
+    void linkLast (Task task) {
+
+        final Node oldTail = tail;
+        final Node newNode = new Node(tail, task, null);
+        tail = newNode;
+
+        if (oldTail == null) {
+            tail = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+
+        history.put(task.getId(), newNode);
+    }
+
+
+    //метод удаления ноды
+    void removeNode(Node node) {
+        final Node prev = node.prev;
+        final Node next = node.next;
+
+        if(prev == null) {
+            next.prev = null;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            prev.next = null;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+    }
+
+    //возвращает список задач из двусвязного списка
+    public List<Task> getTask() {
+       List <Task> list = new ArrayList<>();
+
+        for (Node<Task>  node : history.values()) {
+            list.add(node.task);
+        }
+
+        return list;
     }
 }
 
--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,32 +1,55 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
 
+    static HistoryManager historyManager;
+
+    @BeforeAll
+    static void beforeAll() {
+        historyManager = Manager.getDefaultHistory();
+        Task task1 = new Task("Задача 1 ", "Задача 1", Status.NEW);
+        Task task2 = new Task("Задача 2 ", "Задача 2", Status.NEW);
+        Task task3 = new Task("Задача 3 ", "Задача 3", Status.NEW);
+        task1.setId(1);
+        task2.setId(2);
+        task3.setId(3);
+        historyManager.addHistory(task1);
+        historyManager.addHistory(task2);
+        historyManager.addHistory(task3);
+
+    }
+
     @Test
     void addHistory() {
-        HistoryManager historyManager = Manager.getDefaultHistory();
-        historyManager.addHistory(new Task("Задача 1 ", "Задача 1", Status.NEW));
         final List<Task> history = historyManager.getHistory();
         assertNotNull(history, "История не пустая.");
-        assertEquals(1, history.size(), "История не пустая.");
+        assertEquals(3, history.size(), "История не пустая.");
 
     }
 
     @Test
-    void maxSizeHistory() {
-        HistoryManager manager = Manager.getDefaultHistory();
+    void removeHistoryByIdTask() {
+        List<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не пустая.");
+        assertEquals(3, history.size(), "История не пустая.");
+        historyManager.remove(1);
+        history = historyManager.getHistory();
+        assertEquals(2, history.size(), "История не пустая.");
 
-        for (int i = 0; i < 15; i++) {
-            manager.addHistory(new Task());
-        }
-
-        assertEquals(10, manager.getHistory().size());
     }
 
+    @Test
+    void getHistory() {
+        List<Task> history = null;
+        assertNull(history);
+        history = historyManager.getHistory();
+        assertNotNull(history);
+
+    }
 
 }
